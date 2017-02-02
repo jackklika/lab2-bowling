f6e557800f3c356d589e993863910a32688645fb
jhklika <jhklika@uwm.edu>
2017-02-02 11:43:49 -0600

    Made the compiler stop whining about Scoresheet.java

--- src/ScoreSheet.java
+++ src/ScoreSheet.java
@@ -1,32 +1,53 @@
 import java.util.LinkedList;
 
+//-1=error, 0=pending, 1=normal, 2=spare, 3=strike
 
 public class ScoreSheet {
-	public int score;
+	public int score; // total score of the game
+	public int currentFrameIndex; // index of the current frame, alongside 'frames'
+	public Frame currentFrame;
+	public LinkedList<Frame> frames; // list of frames
 	
 	public ScoreSheet() {
-		LinkedList<Frame> frames = new LinkedList<Frame>();{
+		score = 0;
+		frames = new LinkedList<Frame>();{
 			for (int x = 0; x < 10; x++){
 				frames.add(new Frame());
 			}
 		}
+		currentFrameIndex = 1;	
 	}
 	
 	// At the beginning of each frame, 10 pins are reset. 
 	// a frame may knock down a max of 10 pins
 	
-	public void throwBall(int knockedDownPins){
-		// check what frame it's at
+	// Should only throw the ball;
+	public void scoreThrow(int knockedDownPins){
+		
 		
 		// check the throws. If there was a first throw that was a strike, go to the next frame.
-		//    go through the whole list
-		// call the score method, go through all the frames
+		currentFrame = frames.get(currentFrameIndex);
 		
+		if (currentFrame.throwBall(knockedDownPins) == 3){ // is it a strike?
+			// score
+			currentFrameIndex++;
+			return;
+		} else if (currentFrame.throwNum == 2){
+			currentFrameIndex++;			
+		}
+		
+		
+		// call the score method
+		scoreCalculator();
 	}
 	
 	// Calculates the current score
 	public int scoreCalculator(){
+		
+		// iteration of the frames
+		
 		return 0;
+		
 	}
 	
 	
@@ -36,50 +57,38 @@ public class ScoreSheet {
 		public int frameScore;
 		public int pins;
 		public int state;
+		public int throwNum;
 		
 		public Frame(){
 			pins = 10;
-			
-		}
+			throwNum = 1;
+			state = 0;
+		}		
 		
-		public class tThrow {
-			
-			public int throwNum;
-			
-			// num is which throw it is, ie 1 or 2
-			public tThrow(){
-				throwNum = 1;
-				state = 0;
-			}
-			
-			public int throwBall(int pinsKnockedDown){				
-				pins -= pinsKnockedDown;
-				if (throwNum == 1){
-					if (pins <= 0){
-						state = 3;
-						return 3;
-					} else {
-						state = 0;
-						return 0;
-					}
-				} else if (throwNum == 2){
-					if (pins <= 0){
-						state = 2;
-						return 2;
-					} else {
-						state = 1;
-						return 1;
-					}
-					
+		public int throwBall(int pinsKnockedDown){				
+			pins -= pinsKnockedDown;
+			if (throwNum == 1){
+				if (pins <= 0){
+					return state = 3;
+				} else { // not a strike
+					throwNum++;
+					return state = 0;
+				}
+			} else if (throwNum == 2){
+				if (pins <= 0){
+					return state = 2;
 				} else {
-					System.out.println("Error: throwNum != 1 || 2");
-					state = -1;
-					return -1; // returns the state of the throw --
-					//-1=error, 0=pending, 1=normal, 2=spare, 3=strike
+					return state = 1;
 				}
 				
-				
+			} else {
+				System.out.println("Error: throwNum != 1 || 2");
+				return state = -1; // returns the state of the throw --
+				//-1=error, 0=pending, 1=normal, 2=spare, 3=strike
 			}
+			
+				
+			
 			
 		}
 		
