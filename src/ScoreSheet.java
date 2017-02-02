f1cadee5d1e367fe6f17294ee3fd03bc0144507f
jhklika <jhklika@uwm.edu>
2017-02-02 16:36:39 -0600

    Simplified scoring

--- src/ScoreSheet.java
+++ src/ScoreSheet.java
@@ -8,10 +8,13 @@ public class ScoreSheet {
 	public Frame currentFrame;
 	public LinkedList<Frame> frames; // list of frames
 	
+	// Constructor: When the scoresheet is first created, these variables are set.
 	public ScoreSheet() {
 		score = 0;
+		
+		// Take note of the variable's values that the frame constructor sets.
 		frames = new LinkedList<Frame>();{
-			for (int x = 0; x < 12; x++){ // 12 FRAMES BECAUSE WE NEED TO HANDLE A 10th FRAME STRIKE.
+			for (int x = 0; x < 12; x++){ // 12 FRAMES BECAUSE WE NEED TO HANDLE A 10th FRAME STRIKE. Just roll with it.
 				frames.add(new Frame());
 			}
 		}
@@ -19,65 +22,87 @@ public class ScoreSheet {
 	}
 	
 	// Calling this method throws the ball once.
-	// Starts at the first frame, first throw. Calling this method multiple times will move to different frames.
+	// First time it is called, it throws at the first frame, frame's first throw. 
+	// Calling this method multiple times will move to different frames and throws.
 	public void scoreThrow(int knockedDownPins){
 		
 		// make sure the current frame is the one we want to be modifying
 		currentFrame = frames.get(currentFrameIndex);
+		
+		// Throw the ball down the lane.
 		currentFrame.throwBall(knockedDownPins);
 		
-		if (currentFrame.state == 3){ // a strike
-			// score
-			currentFrameIndex++;
-			return;
+		
+		// a strike
+		if (currentFrame.state == 3){ 
+			currentFrame = frames.get(++currentFrameIndex);
 			
-		} else if (currentFrame.throwNum == 2){ // is a second throw
-			currentFrameIndex++;
+		// a spare; and assumed to be a second throw	
+		} else if (currentFrame.state == 2){ 
+			currentFrame = frames.get(++currentFrameIndex);
+		
+		// Second throw of a 'normal frame' (ie not a strike, spare, or error)
+		} else if (currentFrame.state == 1){ 
+			currentFrame = frames.get(++currentFrameIndex);
+		
+		// First throw of a 'normal frame'. <10 pins are knocked down.
+		} else if (currentFrame.state == 0) { 
+			// TODO: What do we do here? Nothing?
+			
+		// -1 or other	
+		} else {
+			System.out.println("We got problems");
 		}
 		
 		
 		// call the score method
-		scoreCalculator();
+		scoreUpdater();
 	}
 	
-	// Calculates the current score
-	public int scoreCalculator(){
-
-		switch(currentFrame.state){
-		case -1:
-			System.out.println("Error -- state = -1");
-			break;
-		case 0:
-			System.out.println("pending");
-			break;
-		case 1:
-			currentFrame.frameScore = currentFrame.throw1 + currentFrame.throw2;
-			break;
+	// Calculates the current score, and moves the currentFrame and currentFrameIndex to the next frame.
+	private void scoreUpdater(){
 		
-		// Spare: The spare's frame's score = following frame's score + 10
-		case 2:
-			currentFrame.frameScore = 
-				currentFrame.throw1 + currentFrame.throw2 + frames.get(currentFrameIndex+1).frameScore;
-			break;
+		
+		int totalScore = 0; // total score. Variable may be able to be consolidated.
+		int frameIndex = 0; // Index of the frame f 
+		for (Frame f : frames){
 			
-		// Strike: The strike's frame's score (ex. "frame 1") = frame 2's score + frame 3's score + 10;
-		case 3:
-			currentFrame.frameScore = 
-					currentFrame.throw1 + currentFrame.throw2 + 
-					frames.get(currentFrameIndex+1).frameScore + frames.get(currentFrameIndex+2).frameScore;
-			break;
-		default:
-			System.out.println("PROBLEM: state < -1 || state > 3");
-			break;
+			switch(f.state){
+			case -1:
+				System.out.println("Error -- state = -1");
+				break;
+			case 0:
+				System.out.println("pending");
+				break;
+				
+			// Normal: The score of a frame with this state will 
+			case 1:
+				f.frameScore = f.throw1 + f.throw2;
+				break;
+			
+			// Spare: The spare's frame's score = following frame's score + 10
+			case 2:
+				f.frameScore = 
+					f.throw1 + f.throw2 + frames.get(currentFrameIndex+1).frameScore;
+				break;
+				
+			// Strike: The strike's frame's score (ex. "frame 1") = frame 2's score + frame 3's score + 10;
+			case 3:
+				f.frameScore =
+						f.throw1 + f.throw2 + 
+						frames.get(frameIndex+1).frameScore + frames.get(frameIndex+2).frameScore;
+				break;
+			default:
+				System.out.println("PROBLEM: state < -1 || state > 3");
+				break;
+			}
+			
+			// iteration of the frames
+			frameIndex++;
+			totalScore += f.frameScore;
 		}
 		
-		
-		currentFrame.frameScore = currentFrame.throw1 + currentFrame.throw2;
-		
-		// iteration of the frames
-		currentFrameIndex++;
-		
-		return 0;
+		score = totalScore;
 		
 	}
 	
@@ -98,6 +123,8 @@ public class ScoreSheet {
 			throwNum = 1;
 			state = 0;
 			frameScore = 0;
+			throw1 = 0;
+			throw2 = 0;
 		}		
 		
 		public int throwBall(int pinsKnockedDown){				
